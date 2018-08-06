94f645dfcb29102c7f2350ae551a7cffd9a71456
guiadco <guiadco4geekhomeinside@gmail.com>
2018-08-06 20:07:41 +0000

    Test

--- .circleci/deploy-ghpages.sh
+++ .circleci/deploy-ghpages.sh
@@ -15,12 +15,12 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    mkdir out
-    doCompile
-    exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy; just doing a build."
+#    mkdir out
+#    doCompile
+#    exit 0
+#fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
