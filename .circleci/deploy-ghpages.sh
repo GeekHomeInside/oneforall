ce5bb62bb217a8fa0857a6d4c9c6085e2041f2a0
guiadco <guiadco4geekhomeinside@gmail.com>
2018-08-07 16:02:30 +0200

    Update deploy script

--- .circleci/deploy-ghpages.sh
+++ .circleci/deploy-ghpages.sh
@@ -42,15 +42,19 @@ cd ..
 # Run our compile script
 doCompile
 
+ls -ltra .
+
+git status
+
 # Now let's go have some fun with the cloned repo
 git config user.name "$GH_NAME"
 git config user.email "$GH_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+#if git diff --quiet; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
