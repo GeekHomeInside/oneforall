0d4293f0d2e711546abf16e0cadb89029cf7e25a
guiadco <guiadco@geekhomeinside.com>
2018-11-01 20:29:11 +0100

    Fix Security Bench

--- .gitlabci/security-bench.sh
+++ .gitlabci/security-bench.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
-docker run -it --net host --pid host --userns host --cap-add audit_control \
+set -xe
+
+docker run --net host --pid host --userns host --cap-add audit_control \
             -v /var/lib:/var/lib \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -v /etc/init.d:/etc/init.d \
             -v /etc:/etc --label docker_bench_security \
-            docker/docker-bench-security
+            docker/docker-bench-security 
