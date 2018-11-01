d687bae466f7927d2423e69063b3444cedbb2d8f
guiadco <guiadco@geekhomeinside.com>
2018-11-01 21:01:37 +0100

    Test deploy docs

--- .gitlabci/deploy-ghpages.sh
+++ .gitlabci/deploy-ghpages.sh
@@ -14,14 +14,6 @@ function doCompile {
   rm -rf doc
 }
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ ! -z "$CIRCLE_PULL_REQUEST" -o "$CIRCLE_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    mkdir out
-    doCompile
-    exit 0
-fi
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
@@ -43,7 +35,7 @@ doCompile
 
 # Now let's go have some fun with the cloned repo
 cd out
-git config user.name "Circle CI"
+git config user.name "Gitlab CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
@@ -55,5 +47,5 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+git commit -m "Build from: $CI_SERVER_NAME $CI_PIPELINE_ID & Deploy to GitHub Pages: ${SHA}"
 git push $SSH_REPO $TARGET_BRANCH
