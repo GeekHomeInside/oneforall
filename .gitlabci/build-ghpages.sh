facfc126a486ebb99e863721780e527b682fa656
guiadco <guiadco@geekhomeinside.com>
2018-11-03 15:53:10 +0100

    Fix error on Build-Docs

--- .gitlabci/build-ghpages.sh
+++ .gitlabci/build-ghpages.sh
@@ -14,5 +14,32 @@ function doCompile {
   rm -rf doc
 }
 
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into doc/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git reset --hard
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
 # Run our compile script
 doCompile
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Gitlab CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
